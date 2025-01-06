55f886d6aab58f58d4f5a5822b0382ebb5581d55
emma <emma.lhermitte@hotmail.fr>
2025-01-06 15:51:30 +0100

    arg parsing in create_synthetic_image_database

--- utils/create_synthetic_image_database.py
+++ utils/create_synthetic_image_database.py
@@ -1,23 +1,66 @@
 import numpy as np
 import random
 from pathlib import Path
-import nipype
 from nipype.interfaces import fsl
 import nibabel as nib
 import torch 
 import torchio as tio 
 import os
-import copy
 import torchio as tio
 import json
+import argparse
+
+
+def set_up():
+    parser = argparse.ArgumentParser(description='descr')
+    parser.add_argument('-lo', '--lesion_overlay', default=True, type=bool, help='If True, the lesion masks to be overlaid must be provided')
+    parser.add_argument('-m', '--masks', type=str, help='Stroke lesion masks that will be overlaid on the whole brains parcellations, must end with "mask.nii.gz"')
+    parser.add_argument('-st', '--stroke_t1', type=str, help='Associated brains with stroke lesions, must be synthstripped and end with "T1w.nii.gz". Needed only if the masks are not normalized')
+    parser.add_argument('-l', '--labels', type=str, help='Base whole brain parcellations on which will be overlaid the stroke lesion, must end with "labels.nii.gz"')
+    parser.add_argument('bt', '--base_t1', type=str, help='Associated base brain images on which will be overlaid the stroke lesion, must be synthstripped and end with "T1w.nii.gz". Needed only if not normalized')
+    parser.add_argument('n', '--normalized', default=False, type=bool, help='If True, the masks and labels must be pre-normalized to the same template')
+    parser.add_argument('ngen', '--n_generations', default=False, type=bool, help='Number of generations per image')
+    parser.add_argument('-t', 'template', type=str, help='Template to register the images and masks. Only if normalized is False')
+    parser.add_argument('-o', 'out', type=str, help='Directory to save the synthetic images and normalized images if necessary')
+    args = parser.parse_args()
+
+    return args
+
+
+def main():
+    args = set_up()
+
+    paths_stroke_masks = sorted(args.masks.glob("**/*mask.nii.gz"))
+    paths_labels = sorted(args.labels.glob("**/*labels.nii.gz"))
+    
+    remap_dict = {4:3,5:1,6:4,7:5,8:6,9:7,10:8,11:3,12:3,13:1,14:9,15:10,16:11,17:12,18:5,19:1,20:2,21:3,22:3,23:1,24:4,25:5,26:6,27:7,28:8,29:9,30:10,31:12,32:5,33:13,34:14} # remap candi
+    remap_dict = {1:2,2:1,3:11,4:3,5:4,6:5,7:8,8:7,9:6,10:10,11:12,12:9,13:0,14:0,15:0,16:13,17:14} # remap dbb 
+    
+    if not args.normalized or not args.lo:
+        paths_stroke_t1 = sorted(args.stroke_t1.glob("**/*T1w.nii.gz"))
+        paths_base_t1 = sorted(args.base_t1.glob("**/*T1w.nii.gz"))
+
+        if args.lo:
+            for path_stroke_mask, path_stroke_t1 in zip(paths_stroke_masks, paths_stroke_t1):
+                flt_out_stroke_t1_file, flt_out_stroke_seg_file, flt_stroke_mat_file = normalize_mni(path_stroke_mask, path_stroke_t1, args.template, args.out, "stroke", dict = None)
+            
+            for path_labels, path_base_t1 in zip(paths_labels, paths_base_t1):
+                flt_out_base_t1_file, flt_out_base_seg_file, flt_base_mat_file = normalize_mni(path_labels, path_base_t1, args.template, args.out, "td", dict = None)
+            generate_synth_img_stroke(flt_out_stroke_seg_file, flt_out_base_seg_file, args.out, args.n_generations, remap_dict, "stroke", "base")
+        else:
+            generate_synth_img(paths_labels, args.out, args.n_generations, "synth_base")
+
+    else:
+        generate_synth_img_stroke(paths_stroke_masks, paths_labels, args.out, args.n_generations, remap_dict, "stroke", "base")
+        
+
 
 def get_transform_from_json(json_file):
     with open(json_file) as f:
         transfo_st = json.load(f)
     train_transfo = parse_transform(transfo_st['train_transforms'],'train_transforms')
     motion_transfo = parse_transform(transfo_st['motion_transforms'],'motion_transforms')
-    tioremap = tio.RemapLabels({29:0,30:0,41:2,42:3,43:4,44:5,46:7,47:8,49:10,50:11,51:12,52:13,53:17,54:18,58:26,60:28,61:0,62:0,72:0,77:2,85:0})
-    return tio.Compose([tioremap, train_transfo, motion_transfo])
+    return tio.Compose([train_transfo, motion_transfo])
 
 def parse_transform(t, transfo_name):
     if isinstance(t, list):
@@ -31,11 +74,19 @@ def parse_transform(t, transfo_name):
 
     t_class = getattr(tio.transforms, t['name'])
     return t_class(**attributes)
-    
+
+def make_consecutive(mask):
+    i=0
+    print(np.unique(mask))
+    labels = np.unique(mask)
+    for l in labels:
+        mask = np.where(mask==l, i, mask)
+        i+=1
+    print(np.unique(mask))
+    return mask
+
 def get_subjects_list(list_img, list_seg, sessions_dict):
-
-    assert len(list_img) == len(list_seg)
-
+       
     i=0
     while i < len(list_img):
         path=list_img[i]
@@ -43,20 +94,42 @@ def get_subjects_list(list_img, list_seg, sessions_dict):
         ses = str(path).split("ses-")[-1][:2]
 
         # print(f"{i} : sub {sub}, ses {ses} :")
-
+        print(f"{i} : sub {sub}, ses {ses} :")
         if not sub in sessions_dict.keys():
-            # print("\tSubject not in list")
+            # print("\tSubject not in list")            
             list_img.pop(i)
-            list_seg.pop(i)
+            
         else:
             if not sessions_dict.get(sub) == ses:
                 # print("\tNot the right session")
                 list_img.pop(i)
-                list_seg.pop(i)
+                # list_seg.pop(i)
             else:
                 # print("\tSubject in list, right session")
                 print(f"sub {sub}, ses {ses}")
                 i+=1
+    i=0    
+    while i < len(list_seg):
+        path=list_seg[i]
+        sub = str(path).split("sub-")[-1][:3]
+        ses = str(path).split("ses-")[-1][:2]
+
+        if not sub in sessions_dict.keys():
+            # print("\tSubject not in list")            
+            list_seg.pop(i)
+            
+        else:
+            if not sessions_dict.get(sub) == ses:
+                # print("\tNot the right session")
+                list_seg.pop(i)
+                # list_seg.pop(i)
+            else:
+                # print("\tSubject in list, right session")
+                print(f"sub {sub}, ses {ses}")
+                i+=1
+    
+    assert len(list_img) == len(list_seg)
+    
     return list_img, list_seg
 
 def generate_synth_img(segmentation_paths, out, n, name_db):
@@ -67,9 +140,10 @@ def generate_synth_img(segmentation_paths, out, n, name_db):
     out_labels_paths=[]
     subject_list=[]
     for path in segmentation_paths:
-        file_id = f'sub-{str(path).split(f"sub-")[-1][:3]}'
+        file_id = f'sub-{str(path).split(f"sub-")[-1].split(f"_")[0]}'
+        seg_vol = torch.tensor(nib.load(path).get_fdata()).type(torch.int16).unsqueeze(dim=0)
         subject = tio.Subject(
-                seg=tio.LabelMap(path),
+                seg=tio.LabelMap(tensor=seg_vol),
                 name=file_id
             )
         subject_list.append(subject)
@@ -78,13 +152,19 @@ def generate_synth_img(segmentation_paths, out, n, name_db):
 
     tioDS = tio.SubjectsDataset(subject_list, transform=tio_transfo)
 
-    for i in range(35,35+n): 
+    for i in range(15,15+n): 
         for sub in tioDS:
             out_s = os.path.join(out, sub.name)
             if not os.path.exists(out_s):
                 os.makedirs(out_s)
-            out_synth_img_path = os.path.join(out_s, f'{sub.name}_synth-{i}.nii.gz')     
-            out_label_path = os.path.join(out_s, f'{sub.name}_label-{i}.nii.gz')     
+
+            if len(str(i))==1:
+                ii=f"0{i}"
+            else:
+                ii=str(i)
+
+            out_synth_img_path = os.path.join(out_s, f'{sub.name}_synth-{ii}.nii.gz')     
+            out_label_path = os.path.join(out_s, f'{sub.name}_label-{ii}.nii.gz')     
             out_synth_img_paths_sub=[]
             out_labels_paths_sub=[]
             sub.synth.save(out_synth_img_path)
@@ -101,74 +181,82 @@ def generate_synth_img(segmentation_paths, out, n, name_db):
 
     return out_synth_img_paths, out_labels_paths
 
-def generate_synth_img_stroke(lesion_masks_paths, brain_parc_path, out, n, name_db_1, name_db_2):
+def add_mask_to_parcellation(brain_parc, lesion_masks_paths):
+    random_index = random.randint(0, len(lesion_masks_paths)-1)
+    lesion_mask_path = lesion_masks_paths.pop(random_index)
+    print(f"Removing index {random_index}")
+
+    lesion_mask = np.asarray(nib.load(lesion_mask_path).get_fdata())
+    # On enlève la lésion si background
+    lesion_in_brain = np.where(brain_parc==0 , 0, lesion_mask)
+    print(np.sum(lesion_in_brain))
+    # On test si la lésion est entièrement dans ventricules
+    lesion_in_brain = np.where(brain_parc==4, 0, lesion_in_brain) 
+
+    print(np.sum(lesion_in_brain))
+    if np.sum(lesion_in_brain) == 0:
+        print("no lesion left")
+        add_mask_to_parcellation(brain_parc, lesion_masks_paths)
+
+    print(len(np.unique(brain_parc)))
+    whole_brain_and_lesion_seg = np.where(lesion_in_brain==1, len(np.unique(brain_parc)), brain_parc)
+    whole_brain_and_lesion_seg = np.where(lesion_in_brain==2, len(np.unique(brain_parc))+1, whole_brain_and_lesion_seg)
+    whole_brain_and_lesion_seg = torch.tensor(whole_brain_and_lesion_seg).type(torch.int16).unsqueeze(dim=0)
+    return whole_brain_and_lesion_seg,  lesion_mask_path, lesion_masks_paths
+
+def generate_synth_img_stroke(lesion_masks_paths, brain_parc_path, out, n, remap_dict, name_db_1, name_db_2):
 
     if not os.path.exists(out):
         os.makedirs(out)
 
+    # for brain_parc_path in brain_parc_paths:
     brain_parc_volume = nib.load(brain_parc_path)
-    whole_brain_seg = np.asarray(brain_parc_volume.get_fdata())
-
-    synth_img_paths_sub=[]
-    label_paths_sub = []
+    brain_parc = make_consecutive(np.asarray(brain_parc_volume.get_fdata()))
+    imgs=[]
+    labels=[]
     for i in range(n) : 
-        random_index = random.randint(0, len(lesion_masks_paths)-1)
-        lesion_mask_path = lesion_masks_paths[random_index]
-       
-        print(f"Removing index {random_index}")
-        
-        lesion_subject = str(lesion_mask_path).split(f"sub-")[-1][:3]
-        lesion_session = str(lesion_mask_path).split("ses-")[-1][:2]
-        brain_parc_subject = str(brain_parc_path).split(f"sub-")[-1][:3]
-        
-        file_id = f'sub{name_db_2}-{brain_parc_subject}_sub{name_db_1}-{lesion_subject}_ses-{lesion_session}_lesion-overlay'
-        out_s = os.path.join(out, f"sub{name_db_2}-{brain_parc_subject}")
-        if not os.path.exists(out_s):
-            os.makedirs(out_s)
-        
-        mask_file = f'{file_id}_seg-{i}.nii.gz'
-        remmapped_mask_file = f'{file_id}_seg-remmapped-{i}.nii.gz'
-        synth_img_file = f'{file_id}_synth-{i}.nii.gz'
-        remmapped_mask_path = os.path.join(out_s, remmapped_mask_file)
-        mask_path = os.path.join(out_s, mask_file)
-        synth_img_path = os.path.join(out_s, synth_img_file)
-
-        lesion_mask_volume = nib.load(lesion_mask_path)
-        lesion_mask = np.asarray(lesion_mask_volume.get_fdata()).astype(bool)
-        
-        whole_brain_and_lesion_seg = np.where(lesion_mask==1, 64, whole_brain_seg)
-        whole_brain_and_lesion_seg = torch.tensor(whole_brain_and_lesion_seg).type(torch.int16).unsqueeze(dim=0)
+        print(len(lesion_masks_paths))
+        whole_brain_and_lesion_seg, lesion_mask_path, lesion_masks_paths = add_mask_to_parcellation(brain_parc, lesion_masks_paths)
+        lesion_subject = str(lesion_mask_path).split(f"/")[-1].split(f"_")[0]
+        lesion_th = str(lesion_mask_path).split("th-")[-1][:3]
+        brain_parc_subject = str(brain_parc_path).split(f"sub-")[-1].split(f"_")[0]
+        file_id = f'sub{name_db_1}-{brain_parc_subject}-sub{name_db_2}-{lesion_subject}-th-{lesion_th}'
 
         subject = tio.Subject(
-            seg=tio.LabelMap(tensor=whole_brain_and_lesion_seg)
+            seg=tio.LabelMap(tensor=whole_brain_and_lesion_seg),
+            name=file_id
         )
-
-        remapping_left_right = {29:0,30:0,41:2,42:3,43:4,44:5,46:7,47:8,49:10,50:11,51:12,52:13,53:17,54:18,58:26,60:28,61:0,62:0,72:0,77:2,85:0}
-        preproc_label_map_transform_left_right = tio.Compose([
-            tio.RemapLabels(remapping_left_right),
-            tio.SequentialLabels()
-        ])
-        transformed_im_left_right = preproc_label_map_transform_left_right(subject['seg'])
-        subject.add_image(tio.Image(type = tio.LABEL, tensor = transformed_im_left_right.data), "remapped_tissues-left_right")
+        out_s = os.path.join(out, f"sub-{brain_parc_subject}")
+        if not os.path.exists(out_s):
+            os.makedirs(out_s)
+        if len(str(i))==1:
+            ii=f"0{i}"
+        else:
+            ii=str(i)
+        # out_synth_img_path_no_transforms = os.path.join(out_s, f'{subject.name}-{ii}_synth_no_transforms.nii.gz')     
+        out_synth_img_path = os.path.join(out_s, f'{subject.name}-{ii}_synth.nii.gz')     
+        out_label_path = os.path.join(out_s, f'{subject.name}-{ii}_label.nii.gz')            
         
-        rescale_transform = tio.RescaleIntensity(out_min_max=(0, 1), percentiles=(1, 99))
-        simulation_transform = tio.RandomLabelsToImage(label_key="remapped_tissues-left_right", image_key='synthetic_mri', ignore_background=True)#, discretize=True)
-        blurring_transform = tio.RandomBlur(std=0.3)
-        transform = tio.Compose([rescale_transform, simulation_transform, blurring_transform])
-        transformed = transform(subject)
+        # synth_transfo = tio.RandomLabelsToImage(label_key='seg', image_key= "synth", default_mean= [0, 1], default_std=[0.02, 0.1])
+        # gen_syn = tio.Compose([tioremap_dbb, synth_transfo])
 
-        final_img = nib.Nifti1Image(np.asarray(transformed["synthetic_mri"][tio.DATA])[0,:,:,:], brain_parc_volume.affine)  
-        transformed_mask = nib.Nifti1Image(np.asarray(subject["remapped_tissues-left_right"][tio.DATA])[0,:,:,:], brain_parc_volume.affine)
-        final_mask = nib.Nifti1Image(np.asarray(subject["seg"][tio.DATA])[0,:,:,:], brain_parc_volume.affine)  
-        nib.save(final_img, synth_img_path)
-        nib.save(transformed_mask, remmapped_mask_path)
-        nib.save(final_mask, mask_path)
-        print(f"Synthetic image saved in : {synth_img_path}, mask : {mask_path}")
+        # transformed = gen_syn(subject)
+        # transformed.synth.save(out_synth_img_path_no_transforms)
+        remap=tio.RemapLabels(remap_dict)
+        remapped_subject = remap(subject)
+        tio_transfo = get_transform_from_json("./utils/transforms.json")
+        transformed = tio_transfo(remapped_subject)
 
-        synth_img_paths_sub.append(synth_img_path)
-        label_paths_sub.append(mask_path)
+        transformed.synth.save(out_synth_img_path)
+        tioremap_lesion = tio.RemapLabels({14:13})
+        transformed_remaped = tioremap_lesion(transformed)
+        transformed_remaped.seg.save(out_label_path)
 
-    return synth_img_paths_sub, label_paths_sub
+        print(f"Synthetic image saved in : {out_synth_img_path}")
+
+        imgs.append(out_synth_img_path)
+        labels.append(out_label_path)
+    return imgs, labels
 
 def resample_to_one(path, out_dir):
     if not os.path.exists(out_dir):
@@ -182,11 +270,11 @@ def resample_to_one(path, out_dir):
     print(f"Resampled image saved in : {out_path}")
     return out_path
 
-def normalize_mni(path_img, path_seg, path_ref, out, name_db, dict = None):
+def normalize_mni(path_img, path_seg, path_ref, out):
     if not os.path.exists(out):
             os.makedirs(out)
 
-    subject_id = f"sub-{str(path_img).split(f'sub-')[-1][:3].split('.nii.gz')[0]}"
+    subject_id = f"{str(path_img).split(f'/')[-1].split('.nii.gz')[0]}"
 
     # We register a subject to mni and use the transfo mat to register the labels to mni
     print(f"Registering {subject_id} to mni")
@@ -289,48 +377,5 @@ def invert_transform(img_paths, seg_paths, mat, mni_path, out, name_db, dict=Non
         inverted_masks_paths.append(flt_out_seg_file)
         inverted_mat_paths.append(flt_invt_mat_file)
 
-
-# GET ALL IMAGES + MASKS PATH FROM CAP CANDI AND DBB
-path_cap = Path("/home/emma/data/IRM-CAP/derivatives/Brain_extraction/FS-SynthStrip/CAP/")
-path_cap_normalized = Path("/home/emma/data/IRM-CAP/derivatives/normalization_mni")
-path_candi = Path("/home/emma/data/DATABASES/CANDI/derivatives/FREESURFER-synthseg/")
-path_dbb = Path("/home/emma/data/DATABASES/DBB/derivatives/DBB-subjects")
-paths_img_cap = sorted(path_cap.glob("**/*_synthstripped.nii.gz"))
-paths_img_normalized_cap = sorted(path_cap_normalized.glob("**/*_T1w-norm.nii.gz"))
-paths_img_candi = sorted(path_candi.glob("**/*_resampled-t1.nii.gz"))
-paths_img_dbb = sorted(path_dbb.glob("**/*_synthstripped.nii.gz"))
-paths_seg_cap = sorted(path_cap.glob("**/*_mask.nii.gz"))
-paths_seg_normalized_cap = sorted(path_cap_normalized.glob("**/*_mask-norm.nii.gz"))
-paths_seg_candi = sorted(path_candi.glob("**/*_synthseg-pred.nii.gz"))
-paths_seg_dbb = sorted(path_dbb.glob("**/*_parc.nii.gz"))
-
-
-##### Generate synthetic images from healthy subjects 
-# nb_example_per_subject = int(55/ 27)+1
-# print(f"{len(paths_seg_candi)} for 1000 imgs : {nb_example_per_subject} per sub")
-# generate_synth_img(paths_seg_candi, "/home/emma/data/DATABASES/CANDI/derivatives/synthCANDI/synthhealthy/", nb_example_per_subject, "candi")
-
-
-nb_example_per_subject = int(500/ len(paths_img_candi))+1
-print(f"{nb_example_per_subject} images per subject")
-db1 = "cap"
-db2 = "candi"
-
-#### On récupère les chemins de toutes les images de CAP en fct du dictionnaire des sessions
-sessions_dict = {"001":"02", "002":"01", "003":"01", "005":"01", "006":"02", "008":"01", "009":"02", "010":"01","012":"01", "013":"01", "014":"01", "015":"02", "030":"01","031":"02","032": "01", "036":"01","037": "01", "039": "01", "040":"01","041":"01","043": "01", "044":"01", "063": "01", "066":"01","068":"01","069":"01", "070":"01", "071":"01", "072": "01", "073": "01" }
-paths_img_normalized_cap, paths_seg_normalized_cap = get_subjects_list(paths_img_normalized_cap, paths_seg_normalized_cap, sessions_dict)
-
-#### On normalise images + labels sur MNI
-path_ref_mni = "/home/emma/data/Atlases-Templates-TPM/MNI/mni_icbm152_t1_tal_nlin_sym_09a_brain.nii"
-
-for image, seg in zip(paths_img_candi, paths_seg_candi):
-    out_normalized = "/home/emma/data/DATABASES/CANDI/derivatives/normalization_mni"
-    path_t1_normalised_candi, path_parc_normalised_candi, path_mat_candi = normalize_mni(image, seg, path_ref_mni, out_normalized, "candi")
-
-    outpath_synth_mni_candi = "/home/emma/data/DATABASES/CANDI/derivatives/synthCANDI/mni"
-    imgs_synth_candi, labels_candi = generate_synth_img_stroke(paths_seg_normalized_cap, path_parc_normalised_candi, outpath_synth_mni_candi, nb_example_per_subject, "cap", "candi")
-
-    outpath_synth_candi = "/home/emma/data/DATABASES/CANDI/derivatives/synthCANDI/subject-space"
-    invert_transform(imgs_synth_candi, labels_candi, path_mat_candi, image, outpath_synth_candi, "candi")
 
 #'''
